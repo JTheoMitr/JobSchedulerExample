588e3a3233432dd59a576e3da7b226c79033ebe6
John <jmitropoulos@alumni.berklee.edu>
2022-02-26 20:03:59 -0600

    Updated MainActivity class

--- app/src/main/java/android/example/jobschedulerexample/MainActivity.java
+++ app/src/main/java/android/example/jobschedulerexample/MainActivity.java
@@ -2,10 +2,16 @@ package android.example.jobschedulerexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void scheduleJob(View view) {
+        ComponentName componentName = new ComponentName(this, ExampleJobService.class);
+        JobInfo info = new JobInfo.Builder(123, componentName)
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .setPeriodic(15 * 60 * 1000)
+                .build();
+
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        int resultCode = scheduler.schedule(info);
+        if (resultCode == JobScheduler.RESULT_SUCCESS) {
+            Log.d(TAG, "Job has been scheduled successfully");
+        }
     }
 
     public void cancelJob(View view) {
