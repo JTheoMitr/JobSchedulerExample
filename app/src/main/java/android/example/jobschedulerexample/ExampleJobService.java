90f184bb430402261a15d243ea6875dd92f5fcbb
John <jmitropoulos@alumni.berklee.edu>
2022-02-26 19:49:21 -0600

    Initial commit

---
+++ app/src/main/java/android/example/jobschedulerexample/ExampleJobService.java
@@ -0,0 +1,44 @@
+package android.example.jobschedulerexample;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.util.Log;
+
+public class ExampleJobService extends JobService {
+
+    private static final String TAG = "ExampleJobService";
+    private boolean jobCancelled = false;
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        Log.d(TAG, "Job has started");
+        doBackgroundWork(jobParameters);
+
+        return true;
+    }
+
+    private void doBackgroundWork(JobParameters jobParameters) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    Log.d(TAG, "run: " + i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                Log.d(TAG, "Job finished");
+                jobFinished(jobParameters, false);
+
+            }
+        }).start();
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return false;
+    }
+}
